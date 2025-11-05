71e81c35d5a3f35cff200333e857181cbb6143e2
nthndf-tech <nth.ndf@gmail.com>
2025-11-05 16:44:25 +0100

    Clase usuario creado

---
+++ src/Usuario.java
@@ -0,0 +1,17 @@
+public class Usuario {
+    int dni;
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Bievenid@, Usuari@!");
+
+    }
+
+    public static void nombre(String nombre) {
+
+    }
+}
+
+
+
